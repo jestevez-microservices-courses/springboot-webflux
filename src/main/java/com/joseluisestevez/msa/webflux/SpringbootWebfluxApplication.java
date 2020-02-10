d19723e09d61151d83d2d45af1f547816da00f8a
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-10 15:47:13 +0100

    Delete data before creating test data

--- src/main/java/com/joseluisestevez/msa/webflux/SpringbootWebfluxApplication.java
+++ src/main/java/com/joseluisestevez/msa/webflux/SpringbootWebfluxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.joseluisestevez.msa.webflux.dao.ProductDao;
 import com.joseluisestevez.msa.webflux.models.documents.Product;
@@ -19,6 +20,8 @@ public class SpringbootWebfluxApplication implements CommandLineRunner {
 
     @Autowired
     private ProductDao dao;
+    @Autowired
+    private ReactiveMongoTemplate reactiveMongoTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebfluxApplication.class, args);
@@ -26,6 +29,9 @@ public class SpringbootWebfluxApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        reactiveMongoTemplate.dropCollection("products").subscribe();
+
         Flux.just(new Product("TV Panasonic Pantalla LCD", 456.89), new Product("Sony Camara HD Digital", 177.89), new Product("Apple iPod", 46.89),
                 new Product("Sony Notebook", 846.89), new Product("Hewlett Packard Multifuncional", 200.89), new Product("Bianchi Bicicleta", 70.89),
                 new Product("HP Notebook Omen 17", 2500.89), new Product("Mica Cómoda 5 Cajones", 150.89),
