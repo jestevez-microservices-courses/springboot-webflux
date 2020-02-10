b3a5b0c7a2f42a32aa4e796bb782e3b798a0fbda
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-10 15:49:19 +0100

    Add createAt in initial data

--- src/main/java/com/joseluisestevez/msa/webflux/SpringbootWebfluxApplication.java
+++ src/main/java/com/joseluisestevez/msa/webflux/SpringbootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.joseluisestevez.msa.webflux;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,8 +37,10 @@ public class SpringbootWebfluxApplication implements CommandLineRunner {
         Flux.just(new Product("TV Panasonic Pantalla LCD", 456.89), new Product("Sony Camara HD Digital", 177.89), new Product("Apple iPod", 46.89),
                 new Product("Sony Notebook", 846.89), new Product("Hewlett Packard Multifuncional", 200.89), new Product("Bianchi Bicicleta", 70.89),
                 new Product("HP Notebook Omen 17", 2500.89), new Product("Mica Cómoda 5 Cajones", 150.89),
-                new Product("TV Sony Bravia OLED 4K Ultra HD", 2255.89)).flatMap(p -> dao.save(p))
-                .subscribe(p -> LOGGER.info("Insert product id= [{}] name=[{}]", p.getId(), p.getName()));
+                new Product("TV Sony Bravia OLED 4K Ultra HD", 2255.89)).flatMap(p -> {
+                    p.setCreateAt(new Date());
+                    return dao.save(p);
+                }).subscribe(p -> LOGGER.info("Insert product id= [{}] name=[{}]", p.getId(), p.getName()));
     }
 
 }
