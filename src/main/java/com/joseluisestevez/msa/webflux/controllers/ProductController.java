6b5f7b4269f1ee94eae564af6c7fcc536b56e518
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-10 16:20:49 +0100

    Using reactive data driver to control back pressure

--- src/main/java/com/joseluisestevez/msa/webflux/controllers/ProductController.java
+++ src/main/java/com/joseluisestevez/msa/webflux/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.joseluisestevez.msa.webflux.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.joseluisestevez.msa.webflux.dao.ProductDao;
 import com.joseluisestevez.msa.webflux.models.documents.Product;
@@ -30,6 +33,22 @@ public class ProductController {
         products.subscribe(product -> LOGGER.info("product: [{}]", product.getName()));
 
         model.addAttribute("products", products);
+        model.addAttribute("title", "Product list");
+        return "list";
+    }
+
+    @GetMapping("/list-data-driver")
+    public String listDataDriver(Model model) {
+        Flux<Product> products = productDao.findAll().map(product -> {
+            product.setName(product.getName().toUpperCase());
+            return product;
+        }).delayElements(Duration.ofSeconds(1));
+
+        products.subscribe(product -> LOGGER.info("product: [{}]", product.getName()));
+
+        model.addAttribute("products", products);
+        model.addAttribute("products", new ReactiveDataDriverContextVariable(products, 1));
+
         model.addAttribute("title", "Product list");
         return "list";
     }
