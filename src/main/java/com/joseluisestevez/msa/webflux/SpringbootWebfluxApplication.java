b727e3d00044996e971b61e1087b2923c16e3f97
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-10 15:33:52 +0100

    Create Document model class

---
+++ src/main/java/com/joseluisestevez/msa/webflux/SpringbootWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxApplication.class, args);
+	}
+
+}
