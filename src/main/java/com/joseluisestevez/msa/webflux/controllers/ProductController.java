4e54e16d41abf14ab6e5c47bdaabdb473e110ce7
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-10 16:12:38 +0100

    Subscribe another observer and modify the reactive stream

--- src/main/java/com/joseluisestevez/msa/webflux/controllers/ProductController.java
+++ src/main/java/com/joseluisestevez/msa/webflux/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package com.joseluisestevez.msa.webflux.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +15,20 @@ import reactor.core.publisher.Flux;
 @Controller
 public class ProductController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     private ProductDao productDao;
 
     @GetMapping({ "/list", "/" })
     public String list(Model model) {
-        Flux<Product> products = productDao.findAll();
+        Flux<Product> products = productDao.findAll().map(product -> {
+            product.setName(product.getName().toUpperCase());
+            return product;
+        });
+
+        products.subscribe(product -> LOGGER.info("product: [{}]", product.getName()));
+
         model.addAttribute("products", products);
         model.addAttribute("title", "Product list");
         return "list";
