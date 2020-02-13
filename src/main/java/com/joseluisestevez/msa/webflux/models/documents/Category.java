d56462802a7841c6592e2048969973cd549e3603
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-13 12:48:18 +0100

    Add Category model and dao

---
+++ src/main/java/com/joseluisestevez/msa/webflux/models/documents/Category.java
@@ -0,0 +1,43 @@
+package com.joseluisestevez.msa.webflux.models.documents;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categories")
+public class Category {
+    @Id
+    private String id;
+
+    private String name;
+
+    public Category() {
+        super();
+    }
+
+    public Category(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
+
+}
